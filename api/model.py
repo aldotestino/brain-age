73457cc5f6542b6db24d388d9900f2e1dc5223c0
aldotestino <aldotestino4@gmail.com>
2024-07-09 00:17:03 +0200

    return range for waterfall shap values

--- api/model.py
+++ api/model.py
@@ -29,23 +29,48 @@ class Model:
 
     base_sv = sv.base_values.tolist()[0]
     formatted_sv = self.format_shap_values(sv)
-    waterfall_sv = self.shap_values_waterfall_format(formatted_sv, limit)
+    waterfall_sv = self.shap_values_waterfall_format(formatted_sv, base_sv, limit)
     brain_sv = self.shap_values_glass_brain_format(formatted_sv)
 
-    return waterfall_sv, brain_sv, base_sv
+    return waterfall_sv, brain_sv
   
 
   def format_shap_values(self, sv):
     return [{'value': x, 'data': y, 'name': z} for x, y, z in zip(sv[0].values, sv[0].data, sv[0].feature_names)]
   
 
-  def shap_values_waterfall_format(self, formatted_sv, limit):
-    ordered_values = sorted(formatted_sv, key=lambda x: abs(x['value']), reverse=True)
+  def shap_values_waterfall_format(self, formatted_sv, base_sv, limit=9):
+    ordered_values = sorted(formatted_sv, key=lambda x: abs(x['value']))
 
-    remaining_values_sum = sum(x['value'] for x in ordered_values[limit:])
+    remaining_values_sum = sum(x['value'] for x in ordered_values[:-limit])
     remaining_features_number = len(ordered_values) - limit
 
-    return [*ordered_values[:limit], {'value': remaining_values_sum, 'data': None, 'name': f"{remaining_features_number} other features"}]
+    last_item = {
+      'value': remaining_values_sum,
+      'data': None,
+      'name': f"{remaining_features_number} other features",
+      'range': [base_sv, base_sv + remaining_values_sum]
+    }
+
+    final_values = []
+    last_range_value = last_item['range'][1]
+
+    for item in ordered_values[-limit:]:
+      range_ = [last_range_value, last_range_value + item['value']]
+
+      final_values.append({
+        'value': item['value'],
+        'data': item['data'],
+        'name': item['name'],
+        'range': range_
+      })
+
+      last_range_value = range_[1]
+
+    final_values.reverse()
+    final_values.append(last_item)
+
+    return final_values
 
 
   def shap_values_glass_brain_format(self, formatted_sv):
@@ -69,7 +94,3 @@ class Model:
       shap_by_region[feature]['regions'][f"{side}.{region}"] = value
 
     return shap_by_region
-    
-
-
-
