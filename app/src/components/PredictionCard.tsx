229e83fab6cfcabafd59075cd05b1ac81898dedc
aldotestino <aldotestino4@gmail.com>
2024-09-02 15:56:01 +0200

    show bga's sign

--- app/src/components/PredictionCard.tsx
+++ app/src/components/PredictionCard.tsx
@@ -1,8 +1,12 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { PredictionWithExplanation } from '@/lib/types';
 import { cn } from '@/lib/utils';
+import { useMemo } from 'react';
 
 function PredictionCard({ prediction, age }: Pick<PredictionWithExplanation, 'prediction'> & {age: number}) {
+
+  const bag = useMemo(() => prediction - age, [prediction, age]);
+
   return (
     <Card className='col-span-3 flex flex-col justify-between'>
       <CardHeader>
@@ -15,7 +19,7 @@ function PredictionCard({ prediction, age }: Pick<PredictionWithExplanation, 'pr
           <span className='text-muted-foreground text-xl font-semibold'>Years</span>
         </p>
         <p>
-          <span className={cn('text-5xl font-bold', Math.abs(age - prediction) < 3 ? 'text-green-600' : 'text-red-600')}>{(prediction - age).toFixed(2)}</span>{' '}
+          <span className={cn('text-5xl font-bold', Math.abs(bag) < 3 ? 'text-green-600' : 'text-red-600')}>{bag > 0 && '+'}{bag.toFixed(2)}</span>{' '}
           <span className='text-muted-foreground text-xl font-semibold'>BAG</span>
         </p>
       </CardContent>
