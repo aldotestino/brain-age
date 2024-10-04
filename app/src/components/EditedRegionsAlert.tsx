c178635e2386c10be89ebc602c999bc6eb2552f7
aldotestino <aldotestino4@gmail.com>
2024-10-04 18:14:20 +0200

    modified regions in accordion

--- app/src/components/EditedRegionsAlert.tsx
+++ app/src/components/EditedRegionsAlert.tsx
@@ -1,94 +1,48 @@
+import { TriangleAlert } from 'lucide-react';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './ui/accordion';
 import { DataChangeSchema, Regions, Sides } from '@/lib/types';
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-import { FileSliders } from 'lucide-react';
-import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card';
 import { useMemo } from 'react';
 import { formatGlassBrainRegion } from '@/lib/utils';
 
-function OtherEditedRegions({ 
-  other, 
-  onSelect  
+function EditedRegionsAlert({
+  dataChange,
+  onSelect
 }: {
-  other: { side: Sides, region: Regions }[],
+  dataChange: DataChangeSchema,
   onSelect: (side: Sides, region: Regions) => void
 }) {
 
+  const modifiedRegions = useMemo(() => Object.entries(dataChange).flatMap(([side, regions]) => 
+    Object.entries(regions)
+      .filter(([, value]) => value.percentage !== 0) // Filter regions with percentage !== 0
+      .map(([region]) => ({ side: side as Sides, region: region as Regions }))
+  ), [dataChange]);
+
   return (
-    <HoverCard>
-      <HoverCardTrigger asChild>
-        <span
-          className="underline cursor-help"
-          tabIndex={0}
-          role="button"
-          aria-haspopup="true"
-        >
-          {other.length} other regions
-        </span>
-      </HoverCardTrigger>
-      <HoverCardContent className="w-60 p-0">
-        <div className="max-h-72 overflow-y-auto p-4">
-          <ul className="space-y-2">
-            {other.map(r => (
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>
+          <div className='flex items-center gap-2'>
+            <TriangleAlert className="h-4 w-4" />
+            <span>{modifiedRegions.length} modified regions</span>
+          </div>
+        </AccordionTrigger>
+        <AccordionContent className='space-y-2 pt-2'>
+          <p className='text-muted-foreground'>These regions have already been modified. To use the original values select the &quot;Base&quot; prediction.</p>
+          <ul className='space-y-1 list-inside list-disc'>
+            {modifiedRegions.map(r => (
               <li 
                 key={`${r.side}_${r.region}`} 
-                className="text-sm hover:underline cursor-pointer capitalize" 
+                className="text-sm hover:underline cursor-pointer" 
                 onClick={() => onSelect(r.side, r.region)}
               >
                 {formatGlassBrainRegion(`${r.side}.${r.region}`)}
               </li>
             ))}
           </ul>
-        </div>
-      </HoverCardContent>
-    </HoverCard>);
-}
-
-function EditedRegionsAlert({ 
-  dataChange, 
-  onSelect 
-}: {
-  dataChange: DataChangeSchema, 
-  onSelect: (side: Sides, region: Regions) => void}
-) {
-
-  const { first, other } = useMemo(() => {
-    const [first, ...other] = Object.entries(dataChange).flatMap(([side, regions]) => 
-      Object.entries(regions)
-        .filter(([, value]) => value.percentage !== 0) // Filter regions with percentage !== 0
-        .map(([region]) => ({ side: side as Sides, region: region as Regions }))
-    );
-    return { first, other };
-  }, [dataChange]);
-
-  if (!first) {
-    return null;
-  }
-
-  return (
-    <Alert>
-      <FileSliders className="h-4 w-4" />
-      <AlertTitle>Heads up!</AlertTitle>
-      <AlertDescription>
-        <p>
-          <span 
-            className='hover:underline cursor-pointer capitalize' 
-            onClick={() => onSelect(first.side, first.region)}
-          >
-            {formatGlassBrainRegion(`${first.side}.${first.region}`)}
-          </span>
-          {' '}
-          {other.length > 0 && 
-            <>
-              and{' '}
-              <OtherEditedRegions other={other} onSelect={onSelect} />
-            </>
-          }
-          {' '}
-          have already been modified.
-          To use the original values select the &quot;Base&quot; prediction.
-        </p>
-      </AlertDescription>
-    </Alert>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   );
 }
 
--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -15,12 +15,12 @@ const config = {
       center: true,
       padding: '2rem',
       screens: {
-        '2xl': '1400px',
-      },
+        '2xl': '1400px'
+      }
     },
     extend: {
       fontFamily: {
-        sans: ['var(--font-sans)', ...fontFamily.sans],
+        sans: ['var(--font-sans)', ...fontFamily.sans]
       },
       colors: {
         border: 'hsl(var(--border))',
@@ -30,58 +30,70 @@ const config = {
         foreground: 'hsl(var(--foreground))',
         primary: {
           DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
+          foreground: 'hsl(var(--primary-foreground))'
         },
         secondary: {
           DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
+          foreground: 'hsl(var(--secondary-foreground))'
         },
         destructive: {
           DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
+          foreground: 'hsl(var(--destructive-foreground))'
         },
         muted: {
           DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
+          foreground: 'hsl(var(--muted-foreground))'
         },
         accent: {
           DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
+          foreground: 'hsl(var(--accent-foreground))'
         },
         popover: {
           DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
+          foreground: 'hsl(var(--popover-foreground))'
         },
         card: {
           DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
+          foreground: 'hsl(var(--card-foreground))'
+        }
       },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        sm: 'calc(var(--radius) - 4px)'
       },
       keyframes: {
         'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
+          from: {
+            height: '0'
+          },
+          to: {
+            height: 'var(--radix-accordion-content-height)'
+          }
         },
         'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
+          from: {
+            height: 'var(--radix-accordion-content-height)'
+          },
+          to: {
+            height: '0'
+          }
         },
-        'shine': {
-          from: { backgroundPosition: '200% 0' },
-          to: { backgroundPosition: '-200% 0' },
+        shine: {
+          from: {
+            backgroundPosition: '200% 0'
+          },
+          to: {
+            backgroundPosition: '-200% 0'
+          }
         }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'shine': 'shine 8s ease-in-out infinite'
-      },
-    },
+        shine: 'shine 8s ease-in-out infinite'
+      }
+    }
   },
   plugins: [require('tailwindcss-animate')],
 } satisfies Config;
