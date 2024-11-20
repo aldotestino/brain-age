5c1d745c9a30d6cb51759d5299027fb2ed2642df
aldotestino <aldotestino4@gmail.com>
2024-11-20 17:56:13 +0100

    fix: update host configuration for Railway and local deployment

--- api/main.py
+++ api/main.py
@@ -8,8 +8,11 @@ train_path = 'df_train.xlsx'
 
 PORT = int(os.environ.get("PORT", 8080))
 
+# use ipv6 if running in Railway
+host = "::" if os.environ.get("RAILWAY_SERVICE_ID") else "0.0.0.0"
+
 if __name__ == "__main__":
     model = Model(model_path=model_path, train_path=train_path)
     app = Api(model=model)
 
-    uvicorn.run(app, host="::", port=PORT)
+    uvicorn.run(app, host=host, port=PORT)
